b1c77af71e8c7cab567e428d850a4a10d9da9720
hezaz <62071557+hezaz@users.noreply.github.com>
2023-02-01 03:24:17 +0100

    vf

--- src/pipex.c
+++ src/pipex.c
@@ -6,76 +6,12 @@
 /*   By: hzaz <hzaz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 18:12:02 by hzaz              #+#    #+#             */
-/*   Updated: 2023/02/01 00:03:06 by hzaz             ###   ########.fr       */
+/*   Updated: 2023/02/01 03:23:34 by hzaz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-//size_t	ft_ultimate_len(char **s)
-//{
-//	size_t	len;
-//	int		i;
-//	int		j;
-
-//	len = 0;
-//	i = 0;
-//	j = 0;
-//	while (s && s[i] && s[i][j])
-//	{
-//		while (s && s[i][j++])
-//			len++;
-//		i++;
-//		j = 0;
-//	}
-//	return (len);
-//}
-
-//char	**read_file(const char	*path)
-//{
-//	int		i;
-//	int		j;
-//	int		fd;
-//	char	**ret;
-
-//	i = 0;
-//	j = 0;
-//	fd = open(path, O_RDONLY);
-//	while (get_next_line(fd))
-//		i++;
-//	ret = malloc(sizeof(char *) * ++i);
-//	if (!ret)
-//		return (NULL);
-//	ret[i] = NULL;
-//	close(fd);
-//	fd = open(path, O_RDONLY);
-//	return (NULL);
-//	while (j < i)
-//		ret[j++] = get_next_line(fd);
-//	return (ret);
-//}
-
-//char	*file_tochar(char **file)
-//{
-//	size_t	len;
-//	size_t	i;
-//	char	*str;
-
-//	i = 0;
-//	len = ft_ultimate_len(file);
-//	str = malloc(sizeof(char) *(len + 1));
-//	if (!str)
-//		return (NULL);
-//	str[len] = '\0';
-//	while (i < len && file && *file)
-//	{
-//		while (**file && *file && file)
-//			str[i++] = (*(*file)++);
-//		(*file)++;
-//	}
-//	return (str);
-//	free(file);
-//}
 
 int	erreur(char c)
 {
@@ -136,7 +72,7 @@ void	exec_cmd( char *envp[], char *cmd)
 void	process_p(char	**av, char **envp,int *fd)
 {
 	int	outf;
-	outf = open(av[4], O_WRONLY, 0777);
+	outf = open(av[4], O_TRUNC | O_CREAT | O_RDWR, 0777);
 
 	if (outf < 0)
 		erreur('o');
